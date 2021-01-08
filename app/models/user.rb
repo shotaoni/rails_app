e4d4f16aa0eff79f6d440da0354931ebee6d8f62
shotaonizuka <syota.oni@gmail.com>
2021-01-08 17:00:28 +0900

    add remember function

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,48 @@
 #  email           :string(255)
 #  name            :string(255)
 #  password_digest :string(255)
+#  remember_digest :string(255)
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
+
+    class << self
+        # ランダムな文字列でトークンを作成
+        def new_token
+          SecureRandom.urlsafe_base64
+        end
+    
+        # 暗号化したトークンを作成
+        def digest(string)
+          cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                        BCrypt::Engine.cost
+          BCrypt::Password.create(string, cost: cost)
+        end
+    end
+    
+    # 暗号化したトークンをremember_digestに代入
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # トークンの照合
+    def authenticated?(remember_token)
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # 永続化の破棄
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
 end
