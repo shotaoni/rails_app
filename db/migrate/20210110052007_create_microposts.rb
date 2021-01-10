504c10a83b97b292523f68eb3e65febabee5fe50
shotaonizuka <syota.oni@gmail.com>
2021-01-10 16:41:10 +0900

    add post show

--- db/migrate/20210110052007_create_microposts.rb
+++ db/migrate/20210110052007_create_microposts.rb
@@ -7,6 +7,6 @@ class CreateMicroposts < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+    add_index :microposts, [:user_id, :created_at]
   end
-  add_index :microposts, [:user_id, :created_at]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,15 @@ User.create(
   [{ name: 'shotaonizuka', email: 'shota.oni@gmail.com', password: 'hogehoge', admin: true, activated: true }]
 )
 
-99.times do |n|
+30.times do |n|
   name = Faker::Name.name
   email = "example-#{n + 1}@example.com"
   password = 'password'
-  User.create!(name: name, email: email, password: password)
+  User.create!(name: name, email: email, password: password, activated: true)
+end
+
+users = User.order(:created_at).take(3)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 6)
+  users.each { |user| user.microposts.create!(content: content) }
 end
